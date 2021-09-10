c60e5b9fece3d883d6b7e24d58a5b20a4e214ff0
Scout McFall <scoutmknz@gmail.com>
2021-09-10 15:53:07 +0000

    fix import in version 2 raffle.py

--- raffle.py
+++ raffle.py
@@ -1,6 +1,6 @@
 """Read customer data from file and run a raffle."""
 
-from random import choice
+import random
 
 
 class Customer(object):
@@ -42,7 +42,7 @@ def get_customers_from_file(customer_file_path):
 def pick_winner(customers):
     """Choose a random winner from list of customers."""
 
-    chosen_customer = choice(customers)
+    chosen_customer = random.choice(customers)
     
     name = chosen_customer.name
     email = chosen_customer.email
@@ -55,3 +55,6 @@ def run_raffle():
 
     customers = get_customers_from_file("customers.txt")
     pick_winner(customers)
+
+if __name__ == "__main__":
+    run_raffle()
--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,22 @@
 """Randomly pick customer and print customer info"""
-
+import random
+from customers import get_customers_from_file
 # Add code starting here
+#import? use customers from customer.py
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+    chosen_customer = random.choice(customers)
+    
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+def run_raffle():
+    """run the weekly raffle"""
+    customer = get_customers_from_file("customers.txt")
+    pick_winner(customer)
+
+if __name__ == "__main__":
+    run_raffle()
